88406502e953271783957d76bce94bc13fd6e1f1
chondominguez <chondominguez55@gmail.com>
2023-06-22 14:45:29 -0300

    Tp terminado

--- README.md
+++ README.md
@@ -25,7 +25,7 @@ valgrind --leak-check=full --track-origins=yes --show-reachable=yes --error-exit
 ---
 ##  Funcionamiento
 
-<br>A continuación voy a explicar las funciones más importantes de Menú.
+A continuación voy a explicar las funciones más importantes de Menú.
 
 Pero primero, el diagrama de memoria:
 
@@ -33,17 +33,21 @@ Pero primero, el diagrama de memoria:
 <img width="60%" src="img/memoria_menu.png">
 </div>
 
-Muchas de las complejidades de mis funciones estan totalmente asociadas a las complejidades del TDA Hash.
+<br>Muchas de las complejidades de mis funciones estan totalmente asociadas a las complejidades del TDA Hash.
 
 Para empezar mi menu consta de un nombre y un hash de comandos.
-Donde un comando consta de su nombre, informacion, informacion de ayuda,una funcion que ejecuta lo que hace y un hash de alias.
+Donde un comando tiene un nombre, informacion, informacion de ayuda, una funcion que ejecuta lo que hace y un hash de alias.
 
 Esta estructura armada me permite tener varios comandos a la vez y tambien otras formas de llamar a un comando.
-A su vez, tener la información al alcance de la mano, tanto como la por defecto como la de ayuda, es importante.
+A su vez, tener la información al alcance de la mano, tanto como la por defecto como la de ayuda.
 
 <br>Función `menu_contiene_comando`
 
 Primero verificamos que el comando pasado por parametro exista mediante la función `hash_obtener`. En caso de no existir, estaríamos hablando de un "alias" o de un comando inexistente. La única forma de verificar esto es recorriendo todo el hash comandos, para luego recorrer el hash alias del comando. Para ello usamos la función iteradora del hash llamada `hash_con_cada_clave` y le pasamos pasamos una función que nos permita recorrer el hash alias. De esta forma, si el comando pasado existe como alias, se va a devolver el comando correspondiente. Caso contrario, vamos a retornar NULL para indicar que no existe.
+
+<div align="center">
+<img width="60%" src="img/menu_buscar_comando.png">
+</div>
 
 <br>Función `menu_ejecutar_comando`
 
@@ -51,16 +55,20 @@ Invocamos la función mencionada arriba. Si nos devolvió un comando, vamos a po
 
 <br>Función `comando_agregar_alias`  
 
-Si el comando pasado por parametro no tiene un hash de alias, se lo vamos a crear con la función `hash_crear`. Una vez creado o si ya tenía mediante la función `hash_insertar` agregamos el alias al hash alias del comando. En el caso de todo haya funcionado, retornamos el comando. Sino, NULL.
+Si el comando pasado por parametro no tiene un hash de alias, se lo vamos a crear con la función `hash_crear`. Una vez creado o si ya tenía, mediante la función `hash_insertar` agregamos el alias al hash alias del comando. En el caso de todo haya funcionado, retornamos el comando. Sino, NULL.
+
+<div align="center">
+<img width="60%" src="img/comando_agregar_alias.png">
+</div>
 
 <br>Función `menu_con_cada_comando`
 
-Esta es la función que itera el menú. Usa totalmente la función `hash_con_cada_clave` que itera el hash.
+Esta es la función que itera el menú. Usa totalmente la función `hash_con_cada_clave` que itera el hash de comandos.
 
 <br>Función `menu_destruir`
 
 Se encarga de liberar toda la memoria ocupada por el menu. Pero para ello, necesitamos destruir los comandos, pero antes de eso vamos a tener que destruir sus alias.
-Para ello usaremos `menu_con_cada_comando` y le pasaremos una función que nos sirva para este caso. Donde esa función se va a encargar de liberar el hash alias y luego liberar el comando. Y por último liberamos el hash comandos y el menu.
+Para esto usaremos `menu_con_cada_comando` y le pasaremos una función que nos sirva para este caso. Donde esa función se va a encargar de liberar el hash alias y luego liberar el comando. Y por último liberamos el hash comandos y el menu.
 
 <br>Bueno, ahora voy a explicar mi implementación en el tp2.
 
@@ -70,8 +78,7 @@ Pero antes, el diagrama de memoria:
 <img width="60%" src="img/memoria_tp2.png">
 </div>
 
-Para empezar mi sistema consta de un contador de ids, un hash de hospitales, el hospital activo y la id del hospital activo.
-
+Para empezar mi sistema consta de un contador de ids, un hash hospitales, el hospital activo y la id del hospital activo.
 Un hospital o en mi caso un centro tiene un hospital, un nombre de hospital, un estado para saber si esta activo o no y una id.
 
 Esta implementación es conveniente debido a que un centro tiene una id y un estado.
@@ -80,11 +87,11 @@ A su vez el sistema sabe cual es el hospital que esta activo y su id.
 
 <br>Función `ejecutar_salir`
 
-Para llevar a caso este función, debemos entender que tenemos que meternos en lo profundo de la estructura para poder ir liberando de a poco. Para ello usamos la función `hash_con_cada_clave` que nos va a permitir iterar el hash de hospitales y le pasamos una función que destruye cada centro. Una vez hecho todo eso, liberamos lo que nos resta, el sistema y el menu.
+Para llevar a cabo esta función, debemos entender que tenemos que meternos en lo profundo de la estructura para poder ir liberando de a poco. Para ello usamos la función `hash_con_cada_clave` que nos va a permitir iterar el hash hospitales y le pasamos una función que destruye cada centro. Una vez hecho todo eso, liberamos el sistema y el menu.
 
 <br>Función `ejecutar_cargar`
 
-Primero hacemos todas las verificaciones necesarias para crear un centro. Una vez, esto vamos a poder crear el centro y agregarlo al hash hospitales del sistema. Para insertar usamos`hash_insertar`.
+Primero hacemos todas las verificaciones necesarias para crear un centro. Una vez esto, vamos a poder crear el centro y agregarlo al hash hospitales del sistema. Para insertar usamos `hash_insertar`.
 
 <br>Función `ejecutar_activar`  
 
@@ -100,18 +107,19 @@ Se checkea todo lo necesario para destruir el hospital activo. Si no hay, la fun
 
 En mi opinión, me parece mucho más útil el uso de un ABB. Ya que puedo armar un árbol de pokemones, donde este organizado en base a la salud de los mismos. Como en un hospital lo que busco mayormente es el pokemon con menor salud,  puedo recorrer el árbol por INORDEN  y voy a tener los pokemones que tengan más prioridad en tema de salud.
 
-El problema de reemplazar dicha estructura, implica que tenga que modificar algunas funciones para el óptimo uso del ABB. Por ejemplo en vez de usar una función para ordenar un vector, puedo usar como mencioné el recorrido INORDEN que me permite tener el vector ordenado por salud y con menor complejidad.
-
-Si no modifico ninguna función evidentemente voy a tener problemas con las pruebas. Ahora, si las funciones las adapto al uso de un ABB lo más seguro es que no haya ningún problema con las mismas. 
+No, de hecho es cambiar el vector de pokemones por un abb. Y la estructura ya estaría.
+Ahora, faltaría adaptar el código a un abb. No hace falta cambiar las firmas ni nada de eso en verdad. Por lo tanto no debería de haber ningún problema si se implementa como es debido.
 
 </br>Tabla de complejidades entre ABB y Lista enlazada
 
 |          Operación           | Lista Enlazada | ABB      |
 |------------------------------|----------------|----------|
 | hospital_crear_desde_archivo | O(n)           | O(log n) |
-| hospital_a_cada_pokemon      | O(n)           | O(log n) |
+| hospital_a_cada_pokemon      | O(n)           | O(n)     |
 | hospital_aceptar_emergencias | O(n)           | O(log n) |
 | hospital_obtener_pokemon     | O(n)           | O(log n) |
-| hospital_destruir            | O(n)           | O(log n) |
+| hospital_destruir            | O(n)           | O(n)     |
+
+Como podemos ver, el uso de un ABB reduce la complejidad de las funciones en su mayoría. Por lo cual, si lo que buscamos es que sea menos complejo el ABB es una muy buena opción.
 
 
--- pruebas_alumno.c
+++ pruebas_alumno.c
@@ -43,17 +43,9 @@ void crear_menu_con_nombre_valido_no_es_error()
 	menu_destruir(menu);
 }
 
-void crear_menu_con_nombre_null_es_error()
-{
-	pa2m_nuevo_grupo("Creando menu con nombre NULL");
-	menu_t *menu = menu_crear(NULL);
-	pa2m_afirmar(!menu, "No se puede crear un menu con nombre NULL");
-}
-
 void pruebas_crear_menu()
 {
 	crear_menu_con_nombre_valido_no_es_error();
-	crear_menu_con_nombre_null_es_error();
 }
 
 void crear_comando_con_datos_validos_no_es_error()
@@ -74,26 +66,9 @@ void crear_comando_con_datos_validos_no_es_error()
 	comando_destruir(cmd);
 }
 
-void crear_comando_con_datos_null_es_error()
-{
-	pa2m_nuevo_grupo("Creando comando con datos NULL");
-	comando_t *cmd = comando_crear(NULL, "Cuenta", "Cuenta hasta dos",
-				       ejecutar_cuenta);
-	pa2m_afirmar(!cmd, "No se puede crear un comando con nombre NULL");
-	cmd = comando_crear("C", NULL, "Cuenta hasta dos", ejecutar_cuenta);
-	pa2m_afirmar(!cmd, "No se puede crear un comando con información NULL");
-	cmd = comando_crear("C", "Cuenta", NULL, ejecutar_cuenta);
-	pa2m_afirmar(
-		!cmd,
-		"No se puede crear un comando con información completa NULL");
-	cmd = comando_crear("C", "Cuenta", "Cuenta hasta dos", NULL);
-	pa2m_afirmar(!cmd, "No se puede crear un comando con una funcion NULL");
-}
-
 void pruebas_crear_comando()
 {
 	crear_comando_con_datos_validos_no_es_error();
-	crear_comando_con_datos_null_es_error();
 }
 
 void agregar_comando_valido_no_es_error()
@@ -119,33 +94,9 @@ void agregar_comando_valido_no_es_error()
 	menu_destruir(menu);
 }
 
-void agregar_comando_null_es_error()
-{
-	pa2m_nuevo_grupo("Agregando comandos null");
-	menu_t *menu = menu_crear("Hospital Pokemon");
-	comando_t *cmd1 = comando_crear(NULL, "Cuenta", "Cuenta hasta dos",
-					ejecutar_cuenta);
-	comando_t *cmd2 =
-		comando_crear("C", NULL, "Cuenta hasta dos", ejecutar_cuenta);
-	comando_t *cmd3 = comando_crear("C", "Cuenta", NULL, ejecutar_cuenta);
-	comando_t *cmd4 =
-		comando_crear("C", "Cuenta", "Cuenta hasta dos", NULL);
-	pa2m_afirmar(!menu_agregar_comando(menu, cmd1),
-		     "No se puede agregar un comando 1 null");
-	pa2m_afirmar(!menu_agregar_comando(menu, cmd2),
-		     "No se puede agregar un comando 2 null");
-	pa2m_afirmar(!menu_agregar_comando(menu, cmd3),
-		     "No se puede agregar un comando 3 null");
-	pa2m_afirmar(!menu_agregar_comando(menu, cmd4),
-		     "No se puede agregar un comando 4 null");
-
-	menu_destruir(menu);
-}
-
 void pruebas_agregar_comando_menu()
 {
 	agregar_comando_valido_no_es_error();
-	agregar_comando_null_es_error();
 }
 
 void buscar_comando_con_nombre_o_alias_valido()
@@ -157,32 +108,30 @@ void buscar_comando_con_nombre_o_alias_valido()
 	menu_agregar_comando(menu, cmd1);
 	comando_agregar_alias(cmd1, "Contar");
 	comando_t *cmd_aux = menu_contiene_comando(menu, "C");
-	pa2m_afirmar(cmd_aux != NULL, "Se buscar el comando C y se encuentra");
+	pa2m_afirmar(cmd_aux != NULL, "Se busca el comando C y se encuentra");
 	pa2m_afirmar(strcmp(comando_nombre(cmd_aux), "C") == 0,
 		     "El nombre del comando es C");
 	cmd_aux = menu_contiene_comando(menu, "Contar");
 	pa2m_afirmar(
 		cmd_aux != NULL,
-		"Se buscar el comando C mediante el alias Contar y se encuentra");
+		"Se busca el comando C mediante el alias Contar y se encuentra");
 	pa2m_afirmar(strcmp(comando_nombre(cmd_aux), "C") == 0,
 		     "El nombre del comando es C");
 
 	menu_destruir(menu);
 }
 
-void buscar_comando_con_nombre_o_alias_null_o_inexistente()
+void buscar_comando_con_nombre_o_alias_inexistente_es_error()
 {
-	pa2m_nuevo_grupo("Buscar comando con nombre o alias null");
+	pa2m_nuevo_grupo("Buscar comando con nombre o alias inexistente");
 	menu_t *menu = menu_crear("Hospital Pokemon");
 	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
 					ejecutar_cuenta);
 	menu_agregar_comando(menu, cmd1);
 
-	comando_t *cmd_aux = menu_contiene_comando(menu, NULL);
-	pa2m_afirmar(!cmd_aux, "Se buscar comando NULL y no se encuentra");
-	cmd_aux = menu_contiene_comando(menu, "Enumerar");
+	comando_t *cmd_aux = menu_contiene_comando(menu, "Enumerar");
 	pa2m_afirmar(!cmd_aux,
-		     "Se buscar el comando Enumerar y no se encuentra");
+		     "Se busca el comando Enumerar y no se encuentra");
 
 	menu_destruir(menu);
 }
@@ -190,7 +139,7 @@ void buscar_comando_con_nombre_o_alias_null_o_inexistente()
 void pruebas_buscar_comando_menu()
 {
 	buscar_comando_con_nombre_o_alias_valido();
-	buscar_comando_con_nombre_o_alias_null_o_inexistente();
+	buscar_comando_con_nombre_o_alias_inexistente_es_error();
 }
 
 void ejecutar_comando_valido_no_es_error()
@@ -213,19 +162,16 @@ void ejecutar_comando_valido_no_es_error()
 	menu_destruir(menu);
 }
 
-void ejecutar_comando_valido_con_cosas_null_o_inexistentes()
+void ejecutar_comando_valido_con_valores_inexistentes()
 {
-	pa2m_nuevo_grupo("Ejecutar comando con función válida");
+	pa2m_nuevo_grupo(
+		"Ejecutar comando con función válida y valores inexistentes");
 	menu_t *menu = menu_crear("Hospital Pokemon");
 	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
 					ejecutar_cuenta);
 	menu_agregar_comando(menu, cmd1);
 
 	size_t n = 0;
-	pa2m_afirmar(
-		menu_ejecutar_comando(menu, NULL, NULL, &n),
-		"No se ejecutó la función de un comando NULL y devolvió true");
-	pa2m_afirmar(n != 2, "No se contó hasta dos");
 
 	pa2m_afirmar(
 		menu_ejecutar_comando(menu, "A", NULL, &n),
@@ -238,7 +184,7 @@ void ejecutar_comando_valido_con_cosas_null_o_inexistentes()
 void pruebas_ejecutar_comando_menu()
 {
 	ejecutar_comando_valido_no_es_error();
-	ejecutar_comando_valido_con_cosas_null_o_inexistentes();
+	ejecutar_comando_valido_con_valores_inexistentes();
 }
 
 void recorrer_todo_el_menu_con_funcion_valida()
@@ -337,36 +283,6 @@ void pruebas_iterador_menu()
 	recorrer_el_menu_con_funcion_false();
 }
 
-void destruir_menu_con_comandos_y_alias_null_no_es_error()
-{
-	pa2m_nuevo_grupo(
-		"Creando menu con 5 comandos y alias null, y destruyendo el menu");
-	menu_t *menu = menu_crear("Hospital Pokemon");
-	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
-					ejecutar_cuenta);
-	comando_t *cmd2 =
-		comando_crear("R", "Resta", "Resta hasta dos", ejecutar_cuenta);
-	comando_t *cmd3 =
-		comando_crear("S", "Suma", "Suma hasta dos", ejecutar_cuenta);
-	comando_t *cmd4 = comando_crear(
-		"M", "Multiplica", "Multiplica hasta dos", ejecutar_cuenta);
-	comando_t *cmd5 = comando_crear("D", "Divide", "Divide hasta dos",
-					ejecutar_cuenta);
-
-	menu_agregar_comando(menu, cmd1);
-	pa2m_afirmar(menu_cantidad(menu) == 1, "La cantidad de comandos es 1");
-	menu_agregar_comando(menu, cmd2);
-	pa2m_afirmar(menu_cantidad(menu) == 2, "La cantidad de comandos es 2");
-	menu_agregar_comando(menu, cmd3);
-	pa2m_afirmar(menu_cantidad(menu) == 3, "La cantidad de comandos es 3");
-	menu_agregar_comando(menu, cmd4);
-	pa2m_afirmar(menu_cantidad(menu) == 4, "La cantidad de comandos es 4");
-	menu_agregar_comando(menu, cmd5);
-	pa2m_afirmar(menu_cantidad(menu) == 5, "La cantidad de comandos es 5");
-
-	menu_destruir(menu);
-}
-
 void destruir_menu_con_comandos_y_alias_no_es_error()
 {
 	pa2m_nuevo_grupo(
@@ -405,6 +321,122 @@ void destruir_menu_con_comandos_y_alias_no_es_error()
 void pruebas_creación_y_destruccion()
 {
 	destruir_menu_con_comandos_y_alias_no_es_error();
+}
+
+void crear_menu_con_nombre_null_es_error()
+{
+	pa2m_nuevo_grupo("Creando menu con nombre NULL");
+	menu_t *menu = menu_crear(NULL);
+	pa2m_afirmar(!menu, "No se puede crear un menu con nombre NULL");
+}
+
+void crear_comando_con_datos_null_es_error()
+{
+	pa2m_nuevo_grupo("Creando comando con datos NULL");
+	comando_t *cmd = comando_crear(NULL, "Cuenta", "Cuenta hasta dos",
+				       ejecutar_cuenta);
+	pa2m_afirmar(!cmd, "No se puede crear un comando con nombre NULL");
+	cmd = comando_crear("C", NULL, "Cuenta hasta dos", ejecutar_cuenta);
+	pa2m_afirmar(!cmd, "No se puede crear un comando con información NULL");
+	cmd = comando_crear("C", "Cuenta", NULL, ejecutar_cuenta);
+	pa2m_afirmar(
+		!cmd,
+		"No se puede crear un comando con información completa NULL");
+	cmd = comando_crear("C", "Cuenta", "Cuenta hasta dos", NULL);
+	pa2m_afirmar(!cmd, "No se puede crear un comando con una funcion NULL");
+}
+
+void agregar_comando_null_es_error()
+{
+	pa2m_nuevo_grupo("Agregando comandos null");
+	menu_t *menu = menu_crear("Hospital Pokemon");
+	comando_t *cmd1 = comando_crear(NULL, "Cuenta", "Cuenta hasta dos",
+					ejecutar_cuenta);
+	comando_t *cmd2 =
+		comando_crear("C", NULL, "Cuenta hasta dos", ejecutar_cuenta);
+	comando_t *cmd3 = comando_crear("C", "Cuenta", NULL, ejecutar_cuenta);
+	comando_t *cmd4 =
+		comando_crear("C", "Cuenta", "Cuenta hasta dos", NULL);
+	pa2m_afirmar(!menu_agregar_comando(menu, cmd1),
+		     "No se puede agregar un comando 1 null");
+	pa2m_afirmar(!menu_agregar_comando(menu, cmd2),
+		     "No se puede agregar un comando 2 null");
+	pa2m_afirmar(!menu_agregar_comando(menu, cmd3),
+		     "No se puede agregar un comando 3 null");
+	pa2m_afirmar(!menu_agregar_comando(menu, cmd4),
+		     "No se puede agregar un comando 4 null");
+
+	menu_destruir(menu);
+}
+
+void buscar_comando_con_nombre_o_alias_null_es_error()
+{
+	pa2m_nuevo_grupo("Buscar comando con nombre o alias null");
+	menu_t *menu = menu_crear("Hospital Pokemon");
+	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
+					ejecutar_cuenta);
+	menu_agregar_comando(menu, cmd1);
+
+	comando_t *cmd_aux = menu_contiene_comando(menu, NULL);
+	pa2m_afirmar(!cmd_aux, "Se busca comando NULL y no se encuentra");
+
+	menu_destruir(menu);
+}
+
+void ejecutar_comando_valido_con_valores_null()
+{
+	pa2m_nuevo_grupo("Ejecutar comando con función válida y valores null");
+	menu_t *menu = menu_crear("Hospital Pokemon");
+	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
+					ejecutar_cuenta);
+	menu_agregar_comando(menu, cmd1);
+
+	size_t n = 0;
+	pa2m_afirmar(
+		menu_ejecutar_comando(menu, NULL, NULL, &n),
+		"No se ejecutó la función de un comando NULL y devolvió true");
+	pa2m_afirmar(n != 2, "No se contó hasta dos");
+
+	menu_destruir(menu);
+}
+
+void destruir_menu_con_comandos_y_alias_null_no_es_error()
+{
+	pa2m_nuevo_grupo(
+		"Creando menu con 5 comandos y alias null, y destruyendo el menu");
+	menu_t *menu = menu_crear("Hospital Pokemon");
+	comando_t *cmd1 = comando_crear("C", "Cuenta", "Cuenta hasta dos",
+					ejecutar_cuenta);
+	comando_t *cmd2 =
+		comando_crear("R", "Resta", "Resta hasta dos", ejecutar_cuenta);
+	comando_t *cmd3 =
+		comando_crear("S", "Suma", "Suma hasta dos", ejecutar_cuenta);
+	comando_t *cmd4 = comando_crear(
+		"M", "Multiplica", "Multiplica hasta dos", ejecutar_cuenta);
+	comando_t *cmd5 = comando_crear("D", "Divide", "Divide hasta dos",
+					ejecutar_cuenta);
+
+	menu_agregar_comando(menu, cmd1);
+	pa2m_afirmar(menu_cantidad(menu) == 1, "La cantidad de comandos es 1");
+	menu_agregar_comando(menu, cmd2);
+	pa2m_afirmar(menu_cantidad(menu) == 2, "La cantidad de comandos es 2");
+	menu_agregar_comando(menu, cmd3);
+	pa2m_afirmar(menu_cantidad(menu) == 3, "La cantidad de comandos es 3");
+	menu_agregar_comando(menu, cmd4);
+	pa2m_afirmar(menu_cantidad(menu) == 4, "La cantidad de comandos es 4");
+	menu_agregar_comando(menu, cmd5);
+	pa2m_afirmar(menu_cantidad(menu) == 5, "La cantidad de comandos es 5");
+
+	menu_destruir(menu);
+}
+
+void pruebas_con_null()
+{
+	crear_menu_con_nombre_null_es_error();
+	crear_comando_con_datos_null_es_error();
+	agregar_comando_null_es_error();
+	buscar_comando_con_nombre_o_alias_null_es_error();
+	ejecutar_comando_valido_con_valores_null();
 	destruir_menu_con_comandos_y_alias_null_no_es_error();
 }
 
@@ -433,6 +465,9 @@ int main()
 
 	pa2m_nuevo_grupo("PRUEBAS DE CREACIÓN Y DESTRUCCIÓN");
 	pruebas_creación_y_destruccion();
+
+	pa2m_nuevo_grupo("PRUEBAS CON NULL");
+	pruebas_con_null();
 
 	return pa2m_mostrar_reporte();
 }
